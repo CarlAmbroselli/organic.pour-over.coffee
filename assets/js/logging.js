5ec3cb454c929726cb20a24c6d42fe977357610f
Carl Ambroselli <git@carl-ambroselli.de>
2017-11-17 16:19:38 +0100

    Remove old logging

--- assets/js/logging.js
+++ assets/js/logging.js
@@ -1,103 +1,103 @@
-window.onerror = function(msg, url, linenumber) {
-    return true;
-}
+// window.onerror = function(msg, url, linenumber) {
+//     return true;
+// }
 
-var user_token = ''
+// var user_token = ''
 
-if (window.localStorage) {
-    var user_token = localStorage.getItem('user_token')
-    if (!user_token || user_token.length == 0) {
-        user_token = (Math.random() * Math.pow(2, 70)).toString(36)
-        try {
-            localStorage.setItem('user_token', user_token)
-        } catch(exception) {
-            user_token = "NO_STORAGE_" + (Math.random() * Math.pow(2, 70)).toString(36)
-        }
-    }
-} else {
-    user_token = "NO_STORAGE_" + (Math.random() * Math.pow(2, 70)).toString(36)
-}
+// if (window.localStorage) {
+//     var user_token = localStorage.getItem('user_token')
+//     if (!user_token || user_token.length == 0) {
+//         user_token = (Math.random() * Math.pow(2, 70)).toString(36)
+//         try {
+//             localStorage.setItem('user_token', user_token)
+//         } catch(exception) {
+//             user_token = "NO_STORAGE_" + (Math.random() * Math.pow(2, 70)).toString(36)
+//         }
+//     }
+// } else {
+//     user_token = "NO_STORAGE_" + (Math.random() * Math.pow(2, 70)).toString(36)
+// }
 
-function findGetParameter(parameterName) {
-    var result = null,
-        tmp = [];
-    location.search
-        .substr(1)
-        .split("&")
-        .forEach(function (item) {
-          tmp = item.split("=");
-          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-        });
-    return result;
-}
+// function findGetParameter(parameterName) {
+//     var result = null,
+//         tmp = [];
+//     location.search
+//         .substr(1)
+//         .split("&")
+//         .forEach(function (item) {
+//           tmp = item.split("=");
+//           if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+//         });
+//     return result;
+// }
 
-var session_token = (Math.random() * Math.pow(2, 70)).toString(36)
+// var session_token = (Math.random() * Math.pow(2, 70)).toString(36)
 
-var campaign = findGetParameter('utm_campaign') || 'no_campaign'
-var product = findGetParameter('product')  || 'no_product'
-var version = '4.0'
-var server = 'https://pourover.ambroselli.tech'
+// var campaign = findGetParameter('utm_campaign') || 'no_campaign'
+// var product = findGetParameter('product')  || 'no_product'
+// var version = '4.0'
+// var server = 'https://pourover.ambroselli.tech'
 
-var start_time = new Date()
+// var start_time = new Date()
 
 
-function logSession() {
-    // Log Session (POST http://localhost:3000/log/session)
+// function logSession() {
+//     // Log Session (POST http://localhost:3000/log/session)
 
-    session = JSON.stringify({
-            "campaign": campaign,
-            "session_token": session_token,
-            "product": product,
-            "token": user_token,
-            "domain": window.location.protocol + '//' + window.location.hostname,
-            "version": version
-        })
+//     session = JSON.stringify({
+//             "campaign": campaign,
+//             "session_token": session_token,
+//             "product": product,
+//             "token": user_token,
+//             "domain": window.location.protocol + '//' + window.location.hostname,
+//             "version": version
+//         })
 
-    // alert(session)
+//     // alert(session)
 
-    jQuery.ajax({
-        url: server + "/log/session",
-        type: "GET",
-        data: {data: session}
-    })
-    .done(function(data, textStatus, jqXHR) {
-        // console.log("HTTP Request Succeeded: " + jqXHR.status);
-        // console.log(data);
-        console.log("Logged session", session)
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        console.log("HTTP Request Failed", textStatus, errorThrown);
-    })
-}
+//     jQuery.ajax({
+//         url: server + "/log/session",
+//         type: "GET",
+//         data: {data: session}
+//     })
+//     .done(function(data, textStatus, jqXHR) {
+//         // console.log("HTTP Request Succeeded: " + jqXHR.status);
+//         // console.log(data);
+//         console.log("Logged session", session)
+//     })
+//     .fail(function(jqXHR, textStatus, errorThrown) {
+//         console.log("HTTP Request Failed", textStatus, errorThrown);
+//     })
+// }
 
-function logAction(action, identifier) {
-    // Log Action (POST http://localhost:3000/log/action)
-    action = JSON.stringify({
-            "action": action || 'default',
-            "session_token": session_token,
-            "identifier": identifier || 'default',
-            "path": window.location.href,
-            "elapsed_time": "" + Math.floor(((new Date()) - start_time) / 1000)
-        })
+// function logAction(action, identifier) {
+//     // Log Action (POST http://localhost:3000/log/action)
+//     action = JSON.stringify({
+//             "action": action || 'default',
+//             "session_token": session_token,
+//             "identifier": identifier || 'default',
+//             "path": window.location.href,
+//             "elapsed_time": "" + Math.floor(((new Date()) - start_time) / 1000)
+//         })
 
-    jQuery.ajax({
-        url: server + "/log/action",
-        type: "GET",
-        data: {data: action}
-    })
-    .done(function(data, textStatus, jqXHR) {
-        // console.log("HTTP Request Succeeded: " + jqXHR.status);
-        // console.log(data);
-        console.log("Logged action", action)
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        console.log("HTTP Request Failed", textStatus, errorThrown);
-    })
-    .always(function() {
-        /* ... */
-    });
-}
+//     jQuery.ajax({
+//         url: server + "/log/action",
+//         type: "GET",
+//         data: {data: action}
+//     })
+//     .done(function(data, textStatus, jqXHR) {
+//         // console.log("HTTP Request Succeeded: " + jqXHR.status);
+//         // console.log(data);
+//         console.log("Logged action", action)
+//     })
+//     .fail(function(jqXHR, textStatus, errorThrown) {
+//         console.log("HTTP Request Failed", textStatus, errorThrown);
+//     })
+//     .always(function() {
+//         /* ... */
+//     });
+// }
 
-// always log the session on load
-logSession()
-logAction("openPage", window.location.href)
+// // always log the session on load
+// logSession()
+// logAction("openPage", window.location.href)
